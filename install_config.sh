3fc06c9e0ab54219ea83dc6f085a08a768a7fe36
NabismoPrimeTime <49233513+NabismoPrimeTime@users.noreply.github.com>
2022-04-20 19:17:16 -0400

    Update install_config.sh

--- install_config.sh
+++ install_config.sh
@@ -8,11 +8,13 @@ echo "Your public IP is: " $first_ip_address
 
 sleep 5
 
-apt-get install wiregaurd -Y
+apt-get install wireguard
 
-cd /etc/wiregaurd
+sleep 10
 
-#umask 077; wg genkey | tee privatekey | wg pubkey > publickey
+cd /etc/wireguard
+
+umask 077; wg genkey | tee privatekey | wg pubkey > publickey
 
 
 systemctl enable wg-quick@wg0
@@ -21,16 +23,25 @@ private_key=($
 
 load_config="
 [Interface]
-PrivateKey = <private_key>
+PrivateKey = <a_private_key>
 Address = 10.0.0.1/24
 ListenPort = 51820
 PostUp = iptables -A FORWARD -i wg0 -j ACCEPT; iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
-PostDown = iptables -D FORWARD -i wg0 -j ACCEPT; iptables -t nat -D POSTROUTING -o eth0 -j MASQUERADE"
+PostDown = iptables -D FORWARD -i wg0 -j ACCEPT; iptables -t nat -D POSTROUTING -o eth0 -j MASQUERADE
+
+[Peer]
+AllowedIPS = 10.0.0.1/24
+PersistentKeepalive = 25
+PublicKey =
+
+
+"
 
 peer_addition
 cat $load_config >> /etc/wireguard/wg0.conf
 
 sed -i "s/a_private_key/$private_key/g" /etc/wireguard/wg0.conf
+
 
 
 read -p "What is the public key of the client?" client_pub_key
