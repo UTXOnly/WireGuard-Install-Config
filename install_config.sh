24f06f39194473ec6ab65354eda866d41b0b28c8
NabismoPrimeTime <49233513+NabismoPrimeTime@users.noreply.github.com>
2022-04-20 20:29:35 -0400

    Update install_config.sh

--- install_config.sh
+++ install_config.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-
-
+#Create variable for host's public IP
 first_ip_address="$(curl -Ls ifconfig.me)"
 
 echo "Your public IP is: " $first_ip_address
@@ -14,40 +13,41 @@ sleep 10
 
 cd /etc/wireguard
 
+#Generate public/private keypair 
 umask 077; wg genkey | tee privatekey | wg pubkey > publickey
 
-
+#Quick enable wg0 interface
 systemctl enable wg-quick@wg0
 
+#Populate wg0.conf w/ config and firewall rules to masquerade client traffic from server
 private_key=($
 
 load_config="
 [Interface]
-PrivateKey = <a_private_key>
+PrivateKey = a_private_key
 Address = 10.0.0.1/24
 ListenPort = 51820
 PostUp = iptables -A FORWARD -i wg0 -j ACCEPT; iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
 PostDown = iptables -D FORWARD -i wg0 -j ACCEPT; iptables -t nat -D POSTROUTING -o eth0 -j MASQUERADE
-
 [Peer]
 AllowedIPS = 10.0.0.1/24
 PersistentKeepalive = 25
 PublicKey =
-
-
 "
 
-peer_addition
-cat $load_config >> /etc/wireguard/wg0.conf
+#Populate begining of config file
+cat $load_config > /etc/wireguard/wg0.conf
 
+#Sed script to replace string w/ variable
 sed -i "s/a_private_key/$private_key/g" /etc/wireguard/wg0.conf
 
-
-
+#Read user input as variable
 read -p "What is the public key of the client?" client_pub_key
 
+#Pipe contents of variable to append wg0.conf
 cat $client_pub_key | >> wg0.conf
 
+#Adjust firewall to allow SSH and wireguardVPN traffic
 ufw allow 22/tcp
 ufw allow 51820/udp
 ufw enable
