d2d6d3cdac6737101de5956bd3a06ddd4ed54ebb
Brian Hartford <bhartford419@gmail.com>
2022-11-20 17:58:17 -0500

    remove sudo

--- install_config.sh
+++ install_config.sh
@@ -32,6 +32,21 @@ else
     
     
 fi
+
+#Populate wg0.conf w/ config and firewall rules to masquerade client traffic from server
+conf_file=/etc/wireguard/wg0.conf
+sudo tee -a >${conf_file} <<EOF
+[Interface]
+PrivateKey = a_private_key
+Address = 10.0.0.0/24
+ListenPort = 51820
+PostUp = iptables -A FORWARD -i wg0 -j ACCEPT; iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
+PostDown = iptables -D FORWARD -i wg0 -j ACCEPT; iptables -t nat -D POSTROUTING -o eth0 -j MASQUERADE
+[Peer]
+AllowedIPS = 10.0.0.0/24
+PersistentKeepalive = 25
+EOF
+
 sudo chown 1003:1003 /etc/wireguard
 sudo chmod 755 /etc/wireguard
 cd /etc/wireguard/
@@ -47,19 +62,7 @@ umask 077; wg genkey | tee privatekey | wg pubkey > publickey
 #Create variable for private key
 private_key=$(< privatekey)
 
-#Populate wg0.conf w/ config and firewall rules to masquerade client traffic from server
-conf_file=/etc/wireguard/wg0.conf
-sudo tee -a >${conf_file} <<EOF
-[Interface]
-PrivateKey = a_private_key
-Address = 10.0.0.0/24
-ListenPort = 51820
-PostUp = iptables -A FORWARD -i wg0 -j ACCEPT; iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
-PostDown = iptables -D FORWARD -i wg0 -j ACCEPT; iptables -t nat -D POSTROUTING -o eth0 -j MASQUERADE
-[Peer]
-AllowedIPS = 10.0.0.0/24
-PersistentKeepalive = 25
-EOF
+
 
 
 #Sed script to replace string w/ variable
