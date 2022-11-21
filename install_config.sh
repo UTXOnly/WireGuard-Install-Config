9087bc6fc51cd9db4f76cdc2bb7c3e076cc7f63b
Brian Hartford <bhartford419@gmail.com>
2022-11-20 19:32:27 -0500

    add sudo bash ln 76

--- install_config.sh
+++ install_config.sh
@@ -73,7 +73,7 @@ sudo apt install ufw
 echo -e "${BRed}Do you want to enable UFW firewall now? (yes/no)${NC}"
 read ANSWER
 if [ $ANSWER == "yes" ]; then
-    sudo bash firewall_rules.sh
+    sudo firewall_rules.sh
 else
 	echo "Not starting UFW firewall"
 fi
