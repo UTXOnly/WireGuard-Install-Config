ed01a3c0ecacb2e8646f3f97912e5efc4f904e6b
Brian Hartford <bhartford419@gmail.com>
2022-11-24 20:53:01 -0500

    adjust pubkey

--- add_pub_key.sh
+++ add_pub_key.sh
@@ -7,7 +7,7 @@ NC='\033[0m' # No Color
 echo -e "\nWould you like to:\n \n${BGreen} 1) Add server public key and IP address to client configuration?${NC} \n \n OR \n \n${BGreen}2) Add client public key to server configuration?${NC} \n \n Please enter 1 or 2 and press ENTER \n" 
 read ANSWER1
 if [ $ANSWER1 == 1 ] ; then
-    echo -e "Please enter server public key then press ENTER: " 
+    echo -e "\nPlease enter server public key then press ENTER: " 
     read WG_server_pub_key
     echo ""
     read -p "Please enter server public IP then press ENTER: " WG_server_ip
