0e5a954248e38d99543e3668e996776114a11f60
Brian Hartford <bhartford419@gmail.com>
2022-11-24 20:15:46 -0500

    adjust pubkey

--- add_pub_key.sh
+++ add_pub_key.sh
@@ -4,7 +4,7 @@ BRed='\033[1;31m'
 BGreen='\033[1;32m'
 NC='\033[0m' # No Color
 
-echo -e "Would you like to:\n \n${BGreen} 1) Add server public key and IP address to client configuration${NC}? \n \n OR \n \n${BGreen}2) Add client public key to server configuration?${NC} \n Please enter 1 or 2 and press ENTER\n" 
+echo -e "Would you like to:\n \n${BGreen} 1) Add server public key and IP address to client configuration${NC}? \n \n OR \n \n${BGreen}2) Add client public key to server configuration?${NC} \n \n Please enter 1 or 2 and press ENTER \n" 
 read ANSWER1
 if [ $ANSWER1 == 1 ] ; then
     read -p "Please enter server public key then press ENTER: " WG_server_pub_key
@@ -14,9 +14,10 @@ if [ $ANSWER1 == 1 ] ; then
     sudo sed -i "s/WG_server_pubkey/$WG_server_pub_key/g" /etc/wireguard/wg0.conf
     echo "Added key and IP"
 elif [ $ANSWER1 == 2 ] ; then
-    read -p "Please enter client public key then press ENTER\n" WG_client_pub_key
+    echo -e "Please enter client public key then press ENTER\n"
+    read WG_client_pub_key
     echo ""
-    echo "PublicKey = $WG_client_pub_key" | sudo tee /etc/wireguard/wg0.conf
+    echo "PublicKey = $WG_client_pub_key" | sudo tee -a /etc/wireguard/wg0.conf
     echo "Added client pub key"
 else
     echo "That was not a valid selection, please run script again"
